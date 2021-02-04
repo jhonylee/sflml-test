d868f5de7c1eb49d65461faf33c7e766c74ac296
Jhony Lee Sousa Gouvêa <jhonyleesousagouvea@gmail.com>
2021-02-04 00:31:24 -0300

    esfera movendo

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    sf::CircleShape shape(100.f,10);
+    int posx = 0,posy = 0;
     shape.setFillColor(sf::Color::Green);
-
+    shape.setPosition(posx,posy);
+    int vel = 3;
     while (window.isOpen())
     {
         sf::Event event;
@@ -13,6 +15,25 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if(event.key.code == sf::Keyboard::Left){
+                    posx-=vel;
+                    shape.setPosition(posx,posy);
+                }
+                if(event.key.code == sf::Keyboard::Right){
+                    posx+=vel;
+                    shape.setPosition(posx,posy);
+                }
+                if(event.key.code == sf::Keyboard::Up){
+                    posy-=vel;
+                    shape.setPosition(posx,posy);                }
+                if(event.key.code == sf::Keyboard::Down){
+                    posy+=vel;
+                    shape.setPosition(posx,posy);
+                }
+            }
+            
         }
 
         window.clear();
